040494a5c0c3b52db6b7470cd69b5fc9a70a1017
Aizxmartin <benny@bennydenver.com>
2025-07-21 12:48:09 -0600

    Add files via upload

--- adjustments.py
+++ adjustments.py
@@ -2,7 +2,11 @@
 def calculate_adjustments(subject_info, row):
     # Stub logic: simple AG SF difference adjustment
     subject_sqft = subject_info.get("sqft", 0)
-    comp_sqft = row.get("Above Grade Finished Area", 0)
+    try:
+        comp_sqft = int(row.get("Above Grade Finished Area", 0))
+    except (ValueError, TypeError):
+        return None, None, None
+
     ag_diff = comp_sqft - subject_sqft
     ag_adj_rate = 40  # $/sf
     total_adj = -ag_diff * ag_adj_rate
